be2cdee4ce9e21a98a650bf1ac3c4c3f53046c80
yikham <yihan.chen.cn@gmail.com>
2022-02-09 16:05:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -40,7 +40,7 @@ const BLOG = {
     },
   },
   
-  /* comment: {
+   comment: {
     // support provider: gitalk, utterances, cusdis
     provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
@@ -61,7 +61,7 @@ const BLOG = {
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
     },
   },
-  */
+  
   isProd: process.env.VERCEL_ENV === 'production', // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 };
 // export default BLOG
